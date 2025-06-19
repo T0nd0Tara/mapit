fd04f69e056d7c89252101645da862f46f614964
T0nd0Tara <5632815@gmail.com>
2025-06-19 22:18:41 +0300

    change dist-electron

--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -20,3 +20,6 @@ electron.contextBridge.exposeInMainWorld("ipcRenderer", {
   // You can expose other APTs you need here.
   // ...
 });
+electron.contextBridge.exposeInMainWorld("electronAPI", {
+  proxyRequest: (config) => electron.ipcRenderer.invoke("proxy-request", config)
+});
