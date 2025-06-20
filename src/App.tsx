c9795818c9f10fb7b063ca5364179cf63b523c9c
T0nd0Tara <5632815@gmail.com>
2025-06-20 17:55:01 +0300

    seperate states of request

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import {
   Card,
   CardContent,
@@ -14,12 +14,12 @@ import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
-  Select,
-  SelectContent,
+  Select, SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { IpcSocketConnectOpts } from "node:net";
 
 enum ViewMethod {
   PRETTY = 'Pretty',
@@ -35,11 +35,32 @@ enum HttpMethod {
   PATCH = 'PATCH',
 }
 
-function RequestConfigTabs() {
+interface IState<T> {
+  value: T,
+  set: (newValue: T) => void,
+}
+
+function useComState<T>(initialState: T): IState<T> {
+  const [value, set] = useState<T>(initialState);
+  return { value, set };
+}
+
+interface IRequest {
+  method: HttpMethod,
+  url: string,
+  params?: { [key: string]: any },
+  body?: any,
+  headers?: { [key: string]: string },
+};
+
+type IRequestState = { [key in keyof IRequest]: IState<IRequest[key]> };
+
+function RequestConfigTabs({ request }: { request: IRequestState }) {
   return (
     <Tabs defaultValue="headers">
       <TabsList>
         <TabsTrigger value="headers">Headers</TabsTrigger>
+        <TabsTrigger value="params">Params</TabsTrigger>
         <TabsTrigger value="body">Body</TabsTrigger>
       </TabsList>
       <TabsContent value="headers">
@@ -67,28 +88,14 @@ function RequestConfigTabs() {
         </Card>
       </TabsContent>
       <TabsContent value="body">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you&apos;ll be logged
-              out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-6">
-            <div className="grid gap-3">
-              <Label htmlFor="tabs-demo-current">Current password</Label>
-              <Input id="tabs-demo-current" type="password" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="tabs-demo-new">New password</Label>
-              <Input id="tabs-demo-new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
+        {(request.method.value !== "GET") && (
+          <Textarea
+            placeholder="Request Body (JSON)"
+            value={request.body.value}
+            onChange={(e) => request.body.set(e.target.value)}
+            className="w-full h-40"
+          />
+        )}
       </TabsContent>
     </Tabs>
 
@@ -96,18 +103,28 @@ function RequestConfigTabs() {
 }
 
 export default function App() {
-  const [method, setMethod] = useState<string>(HttpMethod.GET);
-  const [url, setUrl] = useState("");
-  const [body, setBody] = useState(null);
+  // const [method, setMethod] = useState<string>(HttpMethod.GET);
+  // const [url, setUrl] = useState("");
+  // const [body, setBody] = useState(null);
+  //
+  const request: IRequestState = {
+    url: useComState(""),
+    method: useComState(HttpMethod.GET),
+    params: useComState({}),
+    headers: useComState({}),
+    body: useComState(null),
+  };
   const [response, setResponse] = useState("");
   const [viewMethod, setViewMethod] = useState();
 
   const sendRequest = async () => {
     try {
-      const config = {
-        method,
-        url,
-        ...(method !== "GET" && { data: JSON.parse(body ?? '{}') }),
+      const config: AxiosRequestConfig<any> = {
+        url: request.url.value,
+        method: request.method.value,
+        params: request.params.value,
+        headers: request.headers.value,
+        data: request.body.value,
       };
       const res = await axios.request(config);
       setResponse(JSON.stringify(res.data, null, 2));
@@ -120,7 +137,7 @@ export default function App() {
     <Card className="w-dvw h-dvh">
       <CardContent className="space-y-4 p-4">
         <div className="flex space-x-2">
-          <Select onValueChange={setMethod} defaultValue={HttpMethod.GET}>
+          <Select onValueChange={(value: string) => request.method.set(value as HttpMethod)} defaultValue={HttpMethod.GET}>
             <SelectTrigger>
               <SelectValue />
             </SelectTrigger>
@@ -133,21 +150,13 @@ export default function App() {
           <Input
             type="text"
             placeholder="Enter URL"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
+            value={request.url.value}
+            onChange={(e) => request.url.set(e.target.value)}
             className="flex-grow"
           />
           <Button onClick={sendRequest}>Send</Button>
         </div>
-        {(method !== "GET") && (
-          <Textarea
-            placeholder="Request Body (JSON)"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-            className="w-full h-40"
-          />
-        )}
-        <RequestConfigTabs></RequestConfigTabs>
+        <RequestConfigTabs request={request}></RequestConfigTabs>
         <Textarea
           readOnly
           placeholder="Response will appear here"
