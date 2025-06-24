4fcf0689757e67fbecb03798b0f2a7747a22efea
T0nd0Tara <5632815@gmail.com>
2025-06-24 20:10:13 +0300

    add nodePlyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    nodePolyfills(),
   ],
   base: './',
   resolve: {
