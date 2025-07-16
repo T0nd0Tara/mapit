262179361f4580bc6bf05d75d0bc9600f836522d
T0nd0Tara <5632815@gmail.com>
2025-07-16 09:58:45 +0300

    prettier pw conf

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
+const webServerPort = 4173
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4173',
+    baseURL: `http://localhost:${webServerPort}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -43,8 +44,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run preview',
-    url: 'http://localhost:4173',
-    reuseExistingServer: !process.env.CI,
+    command: `npx vite --port ${webServerPort}`,
+    url: `http://localhost:${webServerPort}`,
+    reuseExistingServer: false, //!process.env.CI,
   },
 });
