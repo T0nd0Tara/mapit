e584edf0a836deed4b928c2d6bbd11585bf551fc
T0nd0Tara <5632815@gmail.com>
2025-07-17 22:30:18 +0300

    add more tests to params table

--- tests/basic-functionality/params.spec.ts
+++ tests/basic-functionality/params.spec.ts
@@ -1,16 +1,71 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
+import { random_string } from '../utils/random';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/')
 })
 
+interface IKeyVal {
+  key: string,
+  value: string,
+}
+
+const getParamsTable = async (page: Page): Promise<Locator> => {
+  await page.getByRole('tab', { name: /Params/ }).click();
+  return page.getByTestId('params-key-value-table').first().locator('tbody');
+}
+
+const addParamRow = async (page: Page, {
+  tableBody,
+  newParam,
+}: {
+  tableBody?: Awaited<ReturnType<typeof getParamsTable>>,
+  newParam?: IKeyVal
+}) => {
+  tableBody ??= await getParamsTable(page);
+  newParam ??= { key: random_string(), value: random_string() };
+  const lastRow = tableBody.locator('tr').last();
+
+  const inputs = lastRow.locator('input')
+  expect(await inputs.count()).toBe(2);
+
+  await Promise.all([
+    inputs.first().fill(newParam.key),
+    inputs.last().fill(newParam.value),
+  ]);
+}
+test('cant remove only row', async ({ page }) => {
+  const tableBody = await getParamsTable(page);
+  const rows = tableBody.locator('tr');
+  expect(await rows.count()).toBe(1)
+
+  const trash = rows.first().locator('td').last()
+  expect(await trash.locator('*').count()).toBe(0)
+});
+test('cant remove last row', async ({ page }) => {
+  const tableBody = await getParamsTable(page);
+
+  for (let i = 0; i < 5; i++) {
+    await addParamRow(page, { tableBody });
+  }
+
+  const trash = tableBody.locator('tr').last().locator('td').last()
+  expect(await trash.locator('*').count()).toBe(0)
+});
+
+test('can delete row', async ({ page }) => {
+  const tableBody = await getParamsTable(page);
+  await addParamRow(page, { tableBody });
+  const numOfRows = await tableBody.locator('tr').count();
+  await tableBody.locator('tr').first().locator('td > :first-child').last().click()
+  expect(await tableBody.locator('tr').count()).toBe(numOfRows - 1)
+});
 test('new row appears on input', async ({ page }) => {
-  await page.getByRole('tab', { name: 'Params (0)' }).click();
-  const table = page.getByTestId('params-key-value-table').first()
-  const tableBody = table.locator('tbody');
+  const tableBody = await getParamsTable(page);
   expect(await tableBody.locator('tr').count()).toBe(1)
 
-  await tableBody.locator('tr input').first().fill('a')
+  await addParamRow(page, { tableBody });
 
   expect(await tableBody.locator('tr').count()).toBe(2)
 });
+
