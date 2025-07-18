92fc80b4c01965f6f5e054c8bbfb94e6101f97c8
T0nd0Tara <5632815@gmail.com>
2025-07-18 23:04:21 +0300

    add test that checks removing params from table

--- tests/basic-functionality/params/params-uri-connection.spec.ts
+++ tests/basic-functionality/params/params-uri-connection.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page, Locator } from '@playwright/test';
-import { random_string } from '../../utils/random';
-import { addParamRow, getParamsTable, IKeyVal } from '../../utils/params';
+import { random_string, random_int } from '../../utils/random';
+import { addParamRow, getParamsTable, IKeyVal, removeNthParamRow } from '../../utils/params';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/')
@@ -22,4 +22,35 @@ test("adding param in table adds it to uri", async ({ page }) => {
     uri += `${concatChar}${newParam.key}=${newParam.value}`
     await expect(page.getByTestId('uri-input')).toHaveValue(uri)
   }
+});
+
+test("removing param from table removes it in the uri", async ({ page }) => {
+  const getFullUri = (uri: string, params: IKeyVal[]) =>
+    `${uri}?${params.map(param => `${param.key}=${param.value}`).join('&')}`;
+
+  const uri = `http://${random_string()}/${random_string()}`
+  const [_, tableBody] = await Promise.all([
+    page.getByTestId('uri-input').fill(uri).then(() =>
+      expect(page.getByTestId('uri-input')).toHaveValue(uri)
+    ),
+    getParamsTable(page),
+  ]);
+
+  const params: IKeyVal[] = []
+
+  const paramsToAdd: number = 5;
+  for (let i = 0; i < paramsToAdd; i++) {
+    const newParam: IKeyVal = { key: random_string(), value: random_string() };
+    params.push(newParam)
+    await addParamRow(page, { tableBody, newParam });
+  }
+
+  await expect(page.getByTestId('uri-input')).toHaveValue(getFullUri(uri, params))
+  const indToRemove = random_int({ max: paramsToAdd })
+  await removeNthParamRow(page, { tableBody, index: indToRemove })
+
+
+  await expect(page.getByTestId('uri-input'))
+    .toHaveValue(getFullUri(uri, params.filter((elem, ind) => ind !== indToRemove)));
+
 });
--- tests/utils/random.ts
+++ tests/utils/random.ts
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export function random_string(length: number = 10) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -5,4 +7,13 @@ export function random_string(length: number = 10) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
+}
+
+export function random_int({ min, max }: { min?: number, max?: number }) {
+  min ??= 0
+  min = _.toInteger(min);
+  max ??= 10
+  max = _.toInteger(max);
+
+  return _.random(min, max, false)
 }
