241884ab26cc690be97f340b15cfbb10250c5cf0
T0nd0Tara <5632815@gmail.com>
2025-08-15 11:23:45 +0300

    create dedicated porviders folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { ThemeProvider } from '@/components/providers/theme-provider.tsx'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import './index.css'
