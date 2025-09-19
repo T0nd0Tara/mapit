01119fba91ef4c8f5107939a9f10a37ea96b772e
T0nd0Tara <5632815@gmail.com>
2025-09-19 18:39:25 +0300

    fix node typing

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -1,3 +1,5 @@
 
 const req = (window as any).require;
-export const fs = req('node:fs').promises;
+export const fs = req('node:fs').promises as typeof import('fs/promises');
+export const os = req('node:os') as typeof import('os');
+export const path = req('node:path') as typeof import('path');
